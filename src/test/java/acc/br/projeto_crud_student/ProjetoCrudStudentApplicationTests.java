16aa1c94484121aa6c368c8e1a53c2c14cea60c6
Rafael Lopes Soares <rafael.lopes.soares@accenture.com>
2023-08-23 13:50:00 -0300

    funcional

---
+++ src/test/java/acc/br/projeto_crud_student/ProjetoCrudStudentApplicationTests.java
@@ -0,0 +1,13 @@
+package acc.br.projeto_crud_student;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoCrudStudentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
